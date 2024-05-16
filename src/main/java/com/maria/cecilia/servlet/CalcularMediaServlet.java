6a182888a63351c6834594f65cb646cc63de3d7c
ernandes <ernandes.souza@gmail.com>
2024-05-16 15:09:52 -0300

    estrutura inicial

--- src/main/java/com/maria/cecilia/servlet/CalcularMediaServlet.java
+++ src/main/java/com/maria/cecilia/servlet/CalcularMediaServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "FormServlet", urlPatterns = "/media")
+@WebServlet(name = "FormServlet", urlPatterns = "/calcular-media")
 public class CalcularMediaServlet extends HttpServlet {
 
     @Override
